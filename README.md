717856b400ec7d3e75059e33b25fb298377a847a
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-11-06 17:53:41 +0100

    Upgrade recipe fixes, more mod compatibility

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 This mod adds tiered backpacks to the game.
 
+Requires [Fabric API](https://modrinth.com/mod/fabric-api) and [Fzzy Config](https://modrinth.com/mod/fzzy-config). [Mod Menu](https://modrinth.com/mod/modmenu) is required to access the in-game config.
+
 ## Features
 
 - Six backpack tiers: Leather -> Copper -> Iron -> Golden -> Diamond -> Netherite
 - Size of each backpack fully configurable up to 16x16 slots, with reasonable defaults out of the box
-- Open backpacks by right-clicking them, or from your inventory or chest slot by pressing a keybind (default B)
+- Open backpacks by right-clicking them, or from your inventory by pressing a keybind (default B)
 - All backpacks except netherite are dyeable like other leather equipment
 - Extensive configuration for toggling equip and open methods and other rules
 
-### Mod support
+### Mod compatibility
 
 - [Accessories](https://modrinth.com/mod/accessories): Allows equipping the backpack as an accessory and opening it by pressing a keybind (default B)
 - [Shulker Box Tooltip](https://modrinth.com/mod/shulkerboxtooltip): Allows previewing backpack contents in the tooltip
+- [ItemSwapper](https://modrinth.com/plugin/itemswapper): Allows quick-swapping with items from the Devices list
+- [Item Descriptions](https://modrinth.com/mod/item-descriptions): Provided item descriptions for backpacks
---
+++ src/main/java/me/pajic/unsophisticated_backpacks/mixin/CraftingMenuMixin.java
@@ -0,0 +1,47 @@
+package me.pajic.unsophisticated_backpacks.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import me.pajic.unsophisticated_backpacks.Main;
+import me.pajic.unsophisticated_backpacks.item.ModItems;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ItemContainerContents;
+import net.minecraft.world.item.crafting.CraftingInput;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(CraftingMenu.class)
+public class CraftingMenuMixin {
+
+    @ModifyExpressionValue(
+            method = "slotChangedCraftingGrid",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/crafting/CraftingRecipe;assemble(Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/core/HolderLookup$Provider;)Lnet/minecraft/world/item/ItemStack;"
+            )
+    )
+    private static ItemStack handleBackpackUpgrade(
+            ItemStack original,
+            @Local CraftingInput input,
+            @Local CraftingRecipe recipe,
+            @Local(ordinal = 1) RecipeHolder<CraftingRecipe> recipeHolder
+    ) {
+        if (
+                original.is(ModItems.BACKPACKS) &&
+                recipeHolder.id().location().getNamespace().equals(Main.MOD_ID) &&
+                recipe instanceof ShapedRecipe
+        ) {
+            input.items().stream().filter(stack -> stack.is(ModItems.BACKPACKS))
+                    .findFirst().ifPresent(stack -> original.set(
+                            DataComponents.CONTAINER,
+                            stack.getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY)
+                    ));
+        }
+        return original;
+    }
+}
