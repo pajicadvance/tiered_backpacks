523edb07c25eac50d875a9992989cb4156d4c912
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-12-30 13:47:05 +0100

    1.0.8

--- src/main/java/me/pajic/tiered_backpacks/mixin/ArmorSlotMixin.java
+++ src/main/java/me/pajic/tiered_backpacks/mixin/ArmorSlotMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import me.pajic.tiered_backpacks.TieredBackpacks;
 import me.pajic.tiered_backpacks.util.BackpackUtil;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ArmorSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemContainerContents;
@@ -18,9 +19,8 @@ public class ArmorSlotMixin {
             method = "mayPickup",
             at = @At("RETURN")
     )
-    private boolean modifyMayPickup(boolean original, @Local ItemStack stack) {
-        return TieredBackpacks.CONFIG.preventUnequipWhenNotEmpty.get() && BackpackUtil.isValidContainerHolder(stack) ?
-                stack.getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY) == ItemContainerContents.EMPTY
-                : original;
+    private boolean modifyMayPickup(boolean original, @Local ItemStack stack, @Local(argsOnly = true) Player player) {
+        return !player.isCreative() && TieredBackpacks.CONFIG.preventUnequipWhenNotEmpty.get() && BackpackUtil.isValidContainerHolder(stack) ?
+                stack.getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY) == ItemContainerContents.EMPTY : original;
     }
 }
